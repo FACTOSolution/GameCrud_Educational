54946185386f1363248252b54d139baa11cf3b33
ppalbuquerque <albpedro897@hotmail.com>
2017-07-27 15:29:26 -0300

    Create the game list view as a class

--- my_steam/urls.py
+++ my_steam/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^games/$', views.GameListView.as_view(), name='games'),
 ]
--- my_steam/views.py
+++ my_steam/views.py
@@ -3,6 +3,16 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from .models import Game
+from django.views import generic
+
+class GameListView(generic.ListView):
+    model = Game
+    context_object_name = 'game_list'
+
+    def get_context_data(self, **kwargs):
+        context = super(GameListView,self).get_context_data(**kwargs)
+        context['extra_data'] = 'Example'
+        return context
 
 def index(request):
     num_games = Game.objects.all().count()
