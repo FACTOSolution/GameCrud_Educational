a580f9d9225de3617d7af77a3b091421f7bc545f
ppalbuquerque <albpedro897@hotmail.com>
2017-07-28 15:44:18 -0300

    Add accounts url and create login template

--- QOSN/settings.py
+++ QOSN/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'QOSN.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
