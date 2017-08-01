0e22347d9a37149097cea6fea3481e451d4de2a2
ppalbuquerque <albpedro897@hotmail.com>
2017-08-01 16:41:04 -0300

    Add url to api

--- my_steam/services.py
+++ my_steam/services.py
@@ -1,2 +1,5 @@
 import requests
 import json
+
+def get_player_apps(steam_id):
+    url = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=9ACA027FB904D3DE671F6DFF07A0C69B&steamid="+ steam_id +"&format=json"
